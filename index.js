3b15e088160a2cd4d546b48131fda0e438b33dfe
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-06-08 19:46:00 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//mutilication
+let num1 =2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+// random
+const random = 1;
+
+// mod
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+//max
+const max = (3,10,5,20);
 
 // Write your code here
